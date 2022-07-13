b2114b8475f7f62293ffd4a79a61cfc0437e4a77
cuspcomputers@gmail.com <stirlingstout>
2022-07-13 11:47:44 +0100

    Fix: replaced exp_print with the code from Henderson's book (see p 299)

--- Print.vb
+++ Print.vb
@@ -2,33 +2,29 @@
 
 
     Sub exp_print(obj As LK_Object)
-        If obj IsNot Nothing Then AndAlso obj.isNotNil() Then
-            ' See comment in LK_Object.IsNil
-            If obj.isNumber() Then
-                Console.Write($"{obj.number} ")
-            ElseIf obj.isSymbol() Then
+        If obj IsNot Nothing Then
+            If obj.isSymbol() Then
                 Console.Write($"{obj.symbol} ")
-            ElseIf obj.isCons() Then
-                If obj.car().isNotCons() AndAlso obj.cdr().isNotCons() AndAlso obj.cdr().isNotNil() Then
-                    ' Dotted pair
-                    exp_print(obj.car())
-                    Console.Write(". ")
-                    exp_print(obj.cdr())
-                Else
-                    If obj.car().isCons() Then
-                        Console.Write("( ")
-                    End If
-                    exp_print(obj.car())
-                    If obj.car().isCons() Then
-                        Console.Write(") ")
-                    End If
-                    If obj IsNot Nothing AndAlso obj.cdr() IsNot Nothing AndAlso obj.cdr().isNotNil() Then
-                        exp_print(obj.cdr())
-                    End If
-                End If
+            ElseIf obj.isNumber() Then
+                Console.Write($"{obj.number} ")
             Else
-                Console.WriteLine($"Unknown type: {obj.typeString()}")
+                Debug.Assert(obj.isCons(), $"Unknown type: {obj.typeString()}")
+
+                Console.Write("( ")
+                Dim p = obj
+                Do While p.isCons()
+                    exp_print(p.car())
+                    p = p.cdr()
+                Loop
+                If p.isSymbol() AndAlso p.symbol = "NIL" Then
+                Else
+                    Console.Write(". ")
+                    exp_print(p)
+                End If
+                Console.Write(") ")
             End If
+        Else
+            Debug.Fail("Trying to print Nothing")
         End If
     End Sub
 
